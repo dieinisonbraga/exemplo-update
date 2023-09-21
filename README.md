8de6d215bca747e12bb99d4d0fcf0841019c1d61
Dieinison <dieinisonbraga@gmail.com>
2023-09-21 13:04:40 -0300

    Commit inicial

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# exemplo-update
-Exemplo de update
+# Exemplo Update
+
+Exemplo de uso de uma operação update, adaptado do código da aula-3 Sprint I (Desenvolvimento FullStack Básico).
---
+++ aula-3/meu_app_front/meu_app_front/scripts.js
@@ -0,0 +1,184 @@
+/*
+  --------------------------------------------------------------------------------------
+  Função para obter a lista existente do servidor via requisição GET
+  --------------------------------------------------------------------------------------
+*/
+const getList = async () => {
+  let url = 'http://127.0.0.1:5000/produtos';
+  fetch(url, {
+    method: 'get',
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      data.produtos.forEach(item => insertList(item.nome, item.quantidade, item.valor))
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
+/*
+  --------------------------------------------------------------------------------------
+  Chamada da função para carregamento inicial dos dados
+  --------------------------------------------------------------------------------------
+*/
+getList()
+
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para colocar um item na lista do servidor via requisição POST
+  --------------------------------------------------------------------------------------
+*/
+const postItem = async (inputProduct, inputQuantity, inputPrice) => {
+  const formData = new FormData();
+  formData.append('nome', inputProduct);
+  formData.append('quantidade', inputQuantity);
+  formData.append('valor', inputPrice);
+
+  let url = 'http://127.0.0.1:5000/produto';
+  fetch(url, {
+    method: 'post',
+    body: formData
+  })
+    .then((response) => response.json())
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para colocar um item na lista do servidor via requisição POST
+  --------------------------------------------------------------------------------------
+*/
+const updateItem = async (inputProduct, inputQuantity, inputPrice) => {
+  const formData = new FormData();
+  formData.append('nome', inputProduct);
+  formData.append('quantidade', inputQuantity);
+  formData.append('valor', inputPrice);
+
+  let url = 'http://127.0.0.1:5000/produto';
+  fetch(url, {
+    method: 'put',
+    body: formData
+  })
+    .then((response) => response.json())
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para criar um botão close para cada item da lista
+  --------------------------------------------------------------------------------------
+*/
+const insertButton = (parent) => {
+  let span = document.createElement("span");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  parent.appendChild(span);
+}
+
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para remover um item da lista de acordo com o click no botão close
+  --------------------------------------------------------------------------------------
+*/
+const removeElement = () => {
+  let close = document.getElementsByClassName("close");
+  // var table = document.getElementById('myTable');
+  let i;
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      let div = this.parentElement.parentElement;
+      const nomeItem = div.getElementsByTagName('td')[0].innerHTML
+      if (confirm("Você tem certeza?")) {
+        div.remove()
+        deleteItem(nomeItem)
+        alert("Removido!")
+      }
+    }
+  }
+}
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para deletar um item da lista do servidor via requisição DELETE
+  --------------------------------------------------------------------------------------
+*/
+const deleteItem = (item) => {
+  console.log(item)
+  let url = 'http://127.0.0.1:5000/produto?nome=' + item;
+  fetch(url, {
+    method: 'delete'
+  })
+    .then((response) => response.json())
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
+// função que verifica se um produto já está na lista/banco
+const verificaProdutoAdicionado = (inputProduct) => {
+  let nomeProdutos = document.getElementsByClassName("nomeProduto");
+  // percorre lista de produtos para saber se vou adicionar um novo, ou atualizar
+  for(produto of nomeProdutos) {
+    // se o produto já está na tabela
+    if (produto.textContent == inputProduct) return true;
+  }
+  // caso contrário
+  return false;
+}
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para adicionar um novo item com nome, quantidade e valor 
+  --------------------------------------------------------------------------------------
+*/
+const newItem = () => {
+  let inputProduct = document.getElementById("newInput").value;
+  let inputQuantity = document.getElementById("newQuantity").value;
+  let inputPrice = document.getElementById("newPrice").value;
+  if (inputProduct === '') {
+    alert("Escreva o nome de um item!");
+  } else if (isNaN(inputQuantity) || isNaN(inputPrice)) {
+    alert("Quantidade e valor precisam ser números!");
+  } else if(verificaProdutoAdicionado(inputProduct)) {
+    console.log(verificaProdutoAdicionado(inputProduct));
+    updateItem(inputProduct, inputQuantity, inputPrice);
+    alert("Item atualizado!");
+    location.reload(); // atualizando a interface com a lista atualizada
+  } else {
+    postItem(inputProduct, inputQuantity, inputPrice)
+    insertList(inputProduct, inputQuantity, inputPrice)
+    alert("Item adicionado!")
+  }
+}
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para inserir items na lista apresentada
+  --------------------------------------------------------------------------------------
+*/
+const insertList = (nameProduct, quantity, price) => {
+  var item = [nameProduct, quantity, price]
+  var table = document.getElementById('myTable');
+  var row = table.insertRow();
+
+  for (var i = 0; i < item.length; i++) {
+    var cel = row.insertCell(i);
+    if ( i == 0 ) cel.setAttribute("class", "nomeProduto"); // adiciona essa classe apenas na primeira propriedade
+    cel.textContent = item[i];
+  }
+  insertButton(row.insertCell(-1))
+  document.getElementById("newInput").value = "";
+  document.getElementById("newQuantity").value = "";
+  document.getElementById("newPrice").value = "";
+
+  removeElement()
+}
